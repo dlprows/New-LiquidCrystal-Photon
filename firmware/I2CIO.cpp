d63fca45e08496013bda87744bd8777b8496d67d
dlprows <prows.daniel@gmail.com>
2016-07-14 22:23:05 -0600

    Update I2CIO.cpp

--- firmware/I2CIO.cpp
+++ firmware/I2CIO.cpp
@@ -159,13 +159,12 @@ int I2CIO::write ( uint8_t value )
       _shadow = ( value & ~(_dirMask) );
 
       Wire.beginTransmission ( _i2cAddr );
-      delay(2);
+      delay(1);
 #if (ARDUINO <  100) && !SPARK
       Wire.send ( _shadow );
 #else
       Wire.write ( _shadow );
 #endif
-      delay(2);
       status = Wire.endTransmission ();
       delay(2);
    }
